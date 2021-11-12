5df5157a551e7cc0a6697cdb1d5a4b41f1f151e7
kiran1125 <makamkiran95@gmail.com>
2021-11-12 20:05:18 +0530

    Added : Validation of Last name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,5 +4,6 @@ public class UserRegistration {
     public static void main(String[] args) {
         ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
         validationOf.validateFirstName();
+        validationOf.validateLastName();
     }
 }
--- src/com/bridgelabz/ValidationOfUserRegistration.java
+++ src/com/bridgelabz/ValidationOfUserRegistration.java
@@ -2,12 +2,32 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+/**
+ * This class is for validation of User registration
+ * Taking user input and checking whether data is in correct format
+ */
 public class ValidationOfUserRegistration {
+    //Scanner class to take the input from the user
     Scanner scanner = new Scanner(System.in);
+
+    /**
+     * this method is to check the first name whether its in correct format are not
+     */
     public void validateFirstName(){
         System.out.println("Enter the First Name");
         String firstName = scanner.next();
         String regex = "^[A-Z][a-z]{2,}$";
         System.out.println(firstName.matches(regex));
+    }
+
+    /**
+     * this method is to check whether the last name entered is correct ot not
+     */
+    public void validateLastName(){
+        System.out.println("Enter the Last Name");
+        String lastName = scanner.next();
+        String regex = "^[A-Z][a-z]{2,}$";
+        System.out.println(lastName.matches(regex));
+
     }
 }
